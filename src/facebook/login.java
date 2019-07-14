e4a941bbfb390ab5a33ce894f82474463b7c521b
gadekarmanoja <gadekarmanoja@gmail.com>
2019-07-14 11:50:40 +0530

    1st commit by dev2

--- src/facebook/login.java
+++ src/facebook/login.java
@@ -5,6 +5,7 @@ public class login {
 	public static void main(String[] args) {
 		System.out.println("hello by Dev1");
 		System.out.println("how are you");
+		System.out.println("hi by dev2");
 
 	}
 
